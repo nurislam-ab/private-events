bc4ec51009bdbdda31d33f98ee3e3c7085bea023
Nurislam Ab <you@example.com>
2020-06-26 22:57:09 +0500

    Add Sessions controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,18 @@ class SessionsController < ApplicationController
     end
 
     def create
-    
+        user = User.find_by(email: params[:session][:email].downcase)
+
+        if user
+            log_in user
+            redirect_to user
+        else
+            render 'new'
+        end
     end
 
     def destroy
-    
+        log_out
+        redirect_to root_url
     end
 end
