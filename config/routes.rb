13aea4261c6fd747f7be800099fc35089549b892
Nurislam Ab <you@example.com>
2020-06-27 01:28:32 +0500

    Add routes for the signin/login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'events/new'
+  get 'events/index'
+  get 'events/show'
+
+  get '/logout', to: 'sessions#destroy'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  
+
   resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
