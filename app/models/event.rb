dc9debb75f64bdebe55538d919c7fa3dbc222a37
Nurislam Ab <you@example.com>
2020-06-30 03:21:24 +0500

    Change column names and add scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User', foreign_key: :user_id
-    has_many :attendances, foreign_key: :event_id
-    has_many :attendees, through: :attendances, source: :attendee
+    has_many :attendances, foreign_key: :attended_event_id
+    has_many :attendees, through: :attendances, source: 'attendee'
+    scope :upcomming, -> { where('date > ?', DateTime.now) }
+    scope :past, -> { where('date < ?', DateTime.now) }
 end
