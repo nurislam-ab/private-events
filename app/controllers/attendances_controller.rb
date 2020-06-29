d6267081602373f8dd3bc5a665e7399d0899c932
Nurislam Ab <you@example.com>
2020-06-30 03:20:25 +0500

    Add create action for the Attendance controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,14 @@
 class AttendancesController < ApplicationController
+    def new
+        @attendance = Attendance.new
+    end
+    
+    def create
+        @user = User.find(params[:user_id])
+        @event = Event.find(params[:event_id])
+
+        @event.attendees << current_user
+
+        redirect_to @event
+    end
 end
