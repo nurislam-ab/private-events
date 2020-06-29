7c732c49a9c75eb6d9b3f8d87f75f30ded8cae79
LOctavio <luis_ram1997@hotmail.com>
2020-06-29 14:44:39 -0500

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
   resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'users#idnex'
+  root 'events#index'
 end
