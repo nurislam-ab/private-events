e1a53b0038082b102cea9102cb58f97367226938
LOctavio <luis_ram1997@hotmail.com>
2020-06-29 16:18:19 -0500

    fix typo

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User', foreign_key: :user_id
-    has_many :attendances, foreign_key: event_id
-    has_many: :attendees, through: :attendances, source: :attendee
+    has_many :attendances, foreign_key: :event_id
+    has_many :attendees, through: :attendances, source: :attendee
 end
