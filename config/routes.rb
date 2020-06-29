32b6f1c16a3de99cff225e4994669a2fc4d4a05d
Nurislam Ab <you@example.com>
2020-06-30 00:12:11 +0500

    Add events to the resources

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   
 
   resources :users, only: [:new, :create, :show]
+  resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'users#idnex'
