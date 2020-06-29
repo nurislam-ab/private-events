8ab5678be9b08f0210de988de371cce93a11c765
Nurislam Ab <you@example.com>
2020-06-30 03:19:35 +0500

    Change column names fro the Attendances table

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2020_06_29_203447) do
 
   create_table "attendances", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "event_id"
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
