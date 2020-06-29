c26f3ff0427f43a1649c794a366e11a306427f49
LOctavio <luis_ram1997@hotmail.com>
2020-06-29 16:18:55 -0500

    migrate table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_203446) do
+ActiveRecord::Schema.define(version: 2020_06_29_203447) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
