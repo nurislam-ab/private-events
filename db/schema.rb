a89018722b6641bcef5f11c06ac01fc3be37c332
Nurislam Ab <you@example.com>
2020-07-01 15:03:46 +0500

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_06_29_203447) do
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
